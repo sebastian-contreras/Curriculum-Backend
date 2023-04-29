7bad2d7c279ee53e003cb7128b3c4228265a0f9d
Sebastian Contreras <Sebasfac2017@gmail.com>
2023-04-28 23:32:13 -0300

    Validaciond de token

--- src/main/java/com/sebastianContreras/Curriculumbackend/config/JwtService.java
+++ src/main/java/com/sebastianContreras/Curriculumbackend/config/JwtService.java
@@ -61,8 +61,21 @@ class JwtService {
 
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+        
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token,Claims::getExpiration);
     }
 }
